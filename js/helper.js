672f10852e563ccf037d6a22e4237dfa72e95cfc
bdshakhawat <mdshakwat.hossain@gmail.com>
2022-11-12 22:34:55 +0600

    button commit

---
+++ js/helper.js
@@ -0,0 +1,110 @@
+
+
+
+function isElementInViewPort(element){
+  let rect = element.getBoundingClientRect();
+  console.log("The bounding Rect of element is ", rect)
+  // get the height of the window 
+  let viewPortBottom = window.innerHeight || document.documentElement.Height;
+  // get the width of the window 
+  let viewPortRight = window.innerWidth || document.documentElement.Width;
+
+  let isTopInViewPort = rect.top <= window.innerHeight-rect.height*2,
+      isLeftInViewPort = rect.left <= window.innerWidth-rect.width*2;
+      // isBottomInViewPort = rect.bottom <= window.height -200 +"px",
+      // isRightInViewPort = rect.right <= window.width -200 +"px";
+
+ // check if element is completely visible inside the viewport
+ return (isTopInViewPort && isLeftInViewPort );
+}
+// let topEle = document.getElementById('top_ele');
+//  console.log("Is top element visible:  ", isElementInViewPort(topEle) )
+
+// let bottomEle = document.getElementById('evil-button');
+// console.log("Is bottom element visible:  ", isElementInViewPort(bottomEle) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findContainment(element, container) {
+
+//     /*
+//     Obtain the bounding rectangle for each element 
+//     */
+//     const brE = element.getBoundingClientRect()
+//     const brC = container.getBoundingClientRect()
+  
+//     /* 
+//     If the boundaries of container pass through the boundaries of
+//     element then classifiy this as an overlap 
+//     */
+//     if (
+//       /* Does container left or right edge pass through element? */
+//       (brE.left < brC.left && brE.right > brC.left) ||
+//       (brE.left < brC.right && brE.right > brC.right) ||
+//       /* Does container top or bottom edge pass through element? */
+//       (brE.top < brC.top && brE.bottom > brC.top) ||
+//       (brE.top < brC.bottom && brE.bottom > brC.bottom)) {
+  
+//       return "overlap";
+//     }
+  
+//     /* 
+//     If boundaries of element fully contained inside bounday of
+//     container, classify this as containment of element in container
+//     */
+//     if (
+//       brE.left >= brC.left &&
+//       brE.top >= brC.top &&
+//       brE.bottom <= brC.bottom &&
+//       brE.right <= brC.right
+//     ) {
+//       return "contained"
+//     }
+  
+//     /* 
+//     Otherwise, the element is fully outside the container 
+//     */
+//     return "outside"
+  
+//   }
+  
+//   const main = document.querySelector(".main")
+//   console.log("A", findContainment(document.querySelector(".a"), main))
+//   console.log("B", findContainment(document.querySelector(".b"), main))
+//   console.log("C", findContainment(document.querySelector(".c"), main))
+//   console.log("D", findContainment(document.querySelector(".d"), main))
+//   console.log("E", findContainment(document.querySelector(".e"), main))
+//   .main {
+//     width: 50%;
+//     height: 200px;
+//     border: 5px solid #000;
+//     position: relative;
+//   }
+  
+//   .obj {
+//     width: 40px;
+//     height: 40px;
+//     border: 1px solid blue;
+//     position: absolute;
+//   }
+//   <div class="main">
+//     <div style="top:105%; left:25%;" class="obj a">A</div>
+//     <div style="top:15%; left:-5%;" class="obj b">B</div>
+//     <div style="top:20%; left:40%;" class="obj c">C</div>
+//     <div style="top:20%; left:110%;" class="obj d">D</div>
+//     <div style="top:90%; left:95%;" class="obj e">E</div>
+//   </div>
--- js/index.js
+++ js/index.js
@@ -1,45 +1,30 @@
 const evilButton=document.getElementById('evil-button');
-            // const OFFSET=100;
-            // evilButton.addEventListener('click',()=>{
-            //     alert("Good Try")
-            //     window.close()
-            // })
+            
+// Main event
+evilButton.addEventListener('mouseover',(e)=>{
 
-            // Main event
-            document.addEventListener('mouseover',(e)=>{
+    let isInWindow = isElementInViewPort(evilButton)
+    console.log(`Is the element in the window ${isInWindow}`)
+    // Step 1: get current position of the element.
+    const buttonBox=evilButton.getBoundingClientRect()
 
-                // Step 1: get current position of the element.
-                const buttonBox=evilButton.getBoundingClientRect()
-                console.log(buttonBox)
+    evilButton.style.position = "absolute";
+    evilButton.style.left = isInWindow ? buttonBox.x + 50 +'px' : buttonBox.x - 500 + 'px';
+    evilButton.style.top = isInWindow ? buttonBox.y + 50 +'px' : buttonBox.y - 500 +'px' ;
 
-                // Step 2: set the element into a new position
-                // evilButton.style.position = "absolute";
-                // evilButton.style.left = buttonBox.x + 50 +'px';
-                // evilButton.style.top = buttonBox.y + 50 +'px';
-                function setNewPosition(x,y){
-                    evilButton.style.position = "absolute";
-                    evilButton.style.left = buttonBox.x + 50 +'px';
-                    evilButton.style.top = buttonBox.y + 50 +'px';
-                }
+    // Step 2: set the element into a new position
+    // function setNewPosition(x,y){
+    //     evilButton.style.position = "absolute";
+    //     evilButton.style.left = buttonBox.x + 50 +'px';
+    //     evilButton.style.top = buttonBox.y + 50 +'px';
+    // }
 
-                // Step 3: check the boundary and keep the element inside the window.
+    // Step 3: check the boundary and keep the element inside the window.
+})
+            
 
-                // const horizontalDistanceFrom=distanceFromCenter(buttonBox.x,x,buttonBox.width)
-                // const verticalDistanceFrom=distanceFromCenter(buttonBox.y,y,buttonBox.height)
-                // const horizontalOffset=buttonBox.width/2 + OFFSET;
-                // const verticalOffset=buttonBox.height/2 + OFFSET;
-
-                // if(Math.abs(horizontalDistanceFrom)<= horizontalOffset && Math.abs(verticalDistanceFrom<=verticalOffset)){
-                //     // setButtonPosition(
-                        
-                //     // )
-                // }
-
-
-                // console.log(buttonBox.x,buttonBox.y)
-            })
-            function distanceFromCenter(boxPosition,mousePosition,boxSize){
-                return boxPosition - mousePosition + boxSize/2
-            }
+function distanceFromCenter(boxPosition,mousePosition,boxSize){
+    return boxPosition - mousePosition + boxSize/2
+}
 
 
